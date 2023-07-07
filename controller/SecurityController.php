3c4a598eb557ea4468baa4eb3e576f0308e3c59c
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 12:40:30 +0200

    register() terminé

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -6,7 +6,6 @@ use App\Session;
 use App\AbstractController;
 use App\ControllerInterface;
 use Model\Managers\UserManager;
-use Model\Managers\PostManager;
 
 class SecurityController extends AbstractController implements ControllerInterface
 {
@@ -27,6 +26,38 @@ class SecurityController extends AbstractController implements ControllerInterfa
                 "users" => $userManager->findAll(["user_registration_date", "DESC"])
             ]
         ];
+    }
+
+    public function register()
+    {
+        if (!isset($_POST["submit"])) {
+            $this->redirectTo('security', 'index');
+        }
+
+        $userManager = new UserManager();
+
+        $nickName = filter_input(INPUT_POST, "nickName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $passwordConfirm = filter_input(INPUT_POST, "passwordConfirm", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+        if (!$nickName || !$password || !$passwordConfirm || !$email) {
+            $this->redirectTo('security', 'index');
+        }
+
+        if ($userManager->findOneByEmail($email) || $userManager->findOneByNickname($nickName)) {
+            $this->redirectTo('security', 'index');
+        }
+
+        if ($password != $passwordConfirm) {
+            $this->redirectTo('security', 'index');
+        }
+
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $userManager->add(["nickName" => $nickName, "password" => $password, "email" => $email]);
+
+        Session::addFlash('success', 'you are registered !');
+        $this->redirectTo('security', 'login');
     }
 
 }
