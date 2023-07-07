948c5e18b304424436bed6af19c735168cd67a5e
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 20:54:45 +0200

    new topic

---
+++ view/forum/newTopic.php
@@ -0,0 +1,12 @@
+<!-- NEW TOPIC -->
+<h3>Add a topic to the category "<?= $category->getCategoryLabel() ?>"</h3>
+
+<form class="form-new-topic" action="index.php?ctrl=topic&action=newTopic&id=<?= $category->getId() ?>" method="POST">
+    <label for="title">Nom du topic</label>
+    <input type="text" name="title" id="title">
+
+    <label for="text">Topic description</label>
+    <textarea rows="5" name="text" id="text"></textarea>
+
+    <input class="button" type="submit" name="submit" id="submit" value="Créer le topic">
+</form>
