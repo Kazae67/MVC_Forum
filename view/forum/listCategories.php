86dd8d5ff43b3665818fe63ccffdd169c2a636f6
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 12:28:38 +0200

    listCategories.php

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,15 +1,7 @@
 <?php
 
-// Au départ, on passe par l'index, qui va nous aiguiller vers une des methodes du controller
-// Puis si on envoie une requette a la BDD, on passera par la couche "Manager" qui
-// va renvoyer une réponse au "Controller" 
-// et le controller va renvoyer une "View"
-
-// dans le dossier view/forum, j'ai toutes mes views qui concernent le forum.
-
 $categories = $result["data"]['categories'];
 
-//(Vérification si le user est admin)
 if (isset($_SESSION["user"]) && ($_SESSION["user"]->getRole()=='admin')) {
     $admin=true;
   }
@@ -17,16 +9,16 @@ if (isset($_SESSION["user"]) && ($_SESSION["user"]->getRole()=='admin')) {
     $admin=false;
   }
 ?>
-<h3>Liste des categories</h3>
+<h3>Categories list</h3>
 
 <?php
 if($admin){
 ?>
     <form  action="index.php?ctrl=category&action=addNewCategory" method="post">
-        <label for="categoryName">Nouvelle catégorie :</label>
-        <input type="text" name="categoryName" id="categoryName"></input>
+        <label for="categoryLabel">New category :</label>
+        <input type="text" name="categoryLabel" id="categoryLabel"></input>
 
-        <input type="submit" name="submit" id="submit" value ="Créer">
+        <input type="submit" name="submit" id="submit" value ="add">
     </form>
 <?php }
   
@@ -35,7 +27,7 @@ if($categories){?>
 foreach ($categories as $category) {
     ?>
     <div class="forum-post-card-category">
-        <p><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$category->getId()?>"><?=$category->getCategoryName()?></a></p>
+        <p><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$category->getId()?>"><?=$category->getcategoryLabel()?></a></p>
     </div>
     <?php
 }
