c4ea63cb6ce8e93670cea745fd15cd9bf93d385b
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 12:00:30 +0200

    restrict ajouté

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,50 +1,26 @@
 <?php
+namespace Controller;
 
-    namespace Controller;
+use App\AbstractController;
+use App\ControllerInterface;
+use Model\Managers\UserManager;
 
-    use App\Session;
-    use App\AbstractController;
-    use App\ControllerInterface;
-    use Model\Managers\UserManager;
-    use Model\Managers\TopicManager;
-    use Model\Managers\PostManager;
-    
-    class HomeController extends AbstractController implements ControllerInterface{
-
-        public function index(){
-            
-           
-                return [
-                    "view" => VIEW_DIR."home.php"
-                ];
-            }
-            
-        
-   
-        public function users(){
-            $this->restrictTo("ROLE_USER");
-
-            $manager = new UserManager();
-            $users = $manager->findAll(['user_registration_date', 'DESC']);
-
-            return [
-                "view" => VIEW_DIR."security/users.php",
-                "data" => [
-                    "users" => $users
-                ]
-            ];
-        }
-
-        public function forumRules(){
-            
-            return [
-                "view" => VIEW_DIR."rules.php"
-            ];
-        }
-
-        /*public function ajax(){
-            $nb = $_GET['nb'];
-            $nb++;
-            include(VIEW_DIR."ajax.php");
-        }*/
+class HomeController extends AbstractController implements ControllerInterface {
+    public function index() {
+        return [
+            "view" => VIEW_DIR . "home.php"
+        ];
     }
+
+    public function users() {
+        $this->restrictTo("ROLE_USER");
+
+        return [
+            "view" => VIEW_DIR . "security/users.php",
+            "data" => [
+                "users" => (new UserManager())->findAll(['user_registration_date', 'DESC'])
+            ]
+        ];
+    }
+}
+?>
