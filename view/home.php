916dd81352eb0a52fe9a5e80c5b96a8541c38771
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 15:38:23 +0200

    petite correction translate

--- view/home.php
+++ view/home.php
@@ -1,9 +1,9 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<h1>WELCOME TO THE FORUM</h1>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
 <p>
-    <a href="view/security/login.php">Se connecter</a>
+    <a href="view/security/login.php">Login</a>
     <span>&nbsp;-&nbsp;</span>
-    <a href="view/security/register.php">S'inscrire</a>
+    <a href="view/security/register.php">Register</a>
 </p>
