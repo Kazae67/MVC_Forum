289ecfb976dcafbf97d84df2f3b47dcc1e602d15
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 16:02:09 +0200

    quelques modifications + ajout label

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,40 +1,36 @@
 <?php
-
+$error = $result["data"]['error'] ?? null;
 $categories = $result["data"]['categories'];
-
-if (isset($_SESSION["user"]) && ($_SESSION["user"]->getRole()=='admin')) {
-    $admin=true;
-  }
-  else{
-    $admin=false;
-  }
+$admin = isset($_SESSION["user"]) && $_SESSION["user"]->getRole() == 'admin';
 ?>
-<h3>Categories list</h3>
 
-<?php
-if($admin){
-?>
-    <form  action="index.php?ctrl=category&action=addNewCategory" method="post">
+<h3>List of categories</h3>
+
+<?php if ($admin) : ?>
+    <form action="index.php?ctrl=category&action=addNewCategory" method="post">
         <label for="categoryLabel">New category :</label>
-        <input type="text" name="categoryLabel" id="categoryLabel"></input>
-
-        <input type="submit" name="submit" id="submit" value ="add">
+        <input type="text" name="categoryLabel" id="categoryLabel">
+        <input type="submit" name="submit" id="submit" value="add">
     </form>
-<?php }
-  
-if($categories){?>
-    <?php
-foreach ($categories as $category) {
-    ?>
-    <div class="forum-post-card-category">
-        <p><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$category->getId()?>"><?=$category->getcategoryLabel()?></a></p>
-    </div>
-    <?php
-}
+<?php endif; ?>
 
-    ?>
-    </tbody>
-</table>
-<?php }?>
-
-  
+<?php if ($categories) : ?>
+    <table class="list-categories">
+        <thead>
+            <tr>
+                <th>Category name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($categories as $category) : ?>
+                <tr>
+                    <td>
+                        <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?= $category->getId() ?>">
+                            <?= $category->getCategoryLabel() ?>
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
