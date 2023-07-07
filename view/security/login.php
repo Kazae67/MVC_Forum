8b28d03113623421687f58c0d344f9829514180d
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 09:26:10 +0200

    loging.php

---
+++ view/security/login.php
@@ -0,0 +1,10 @@
+<form action="index.php?ctrl=security&action=login" method="post">
+
+    <label for="email">Email</label>
+    <input type ="text" name="email" id="email" required></input>
+
+    <label for="password">Password</label>
+    <input type ="password" name="password" id="password" required></input>
+
+    <input class="button" type="submit" name="submit" id="submit" value="login">
+</form>
