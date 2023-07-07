7970358aa109299a4ae3d9f82ee87acde7e834b6
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-07 09:51:46 +0200

    topics list, categories list, register, loging, logout

--- view/layout.php
+++ view/layout.php
@@ -11,43 +11,25 @@
 </head>
 <body>
     <div id="wrapper"> 
-       
         <div id="mainpage">
-            <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
             <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
             <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
             <header>
                 <nav>
                     <div id="nav-left">
-                        <a href="/">Accueil</a>
-                        <?php
-                        if(App\Session::isAdmin()){
-                            ?>
-                            <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
-                          
-                            <?php
-                        }
-                        ?>
+                        <a href="index.php?ctrl=category&action=listCategories">
+                        </a>
+                        <a class="button" href="index.php?ctrl=category&action=listTopics">Topics list</a>
+                        <a class="button" href="index.php?ctrl=category&action=listCategories">Categories list</a>
                     </div>
                     <div id="nav-right">
-                    <?php
-                        
-                        if(App\Session::getUser()){
-                            ?>
-                            <a href="/security/viewProfile.html"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
-                            <a href="/security/logout.html">Déconnexion</a>
-                            <?php
-                        }
-                        else{
-                            ?>
-                            <a href="./view/security/login.php">Connexion</a>
-                            <a href="/security/register.html">Inscription</a>
-                            <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a>
-                        <?php
-                        }
-                   
-                        
-                    ?>
+                        <?php if ($user = App\Session::getUser()): ?>
+                            <a class="button" href="index.php?ctrl=security&action=viewProfile"><?= $user->getNickname() ?></a>
+                            <a class="button" href="index.php?ctrl=security&action=logOut">Logout</a>
+                        <?php else: ?>
+                            <a class="button" href="index.php?ctrl=security&action=login">Login</a>
+                            <a class="button" href="index.php?ctrl=security&action=register">Register</a>
+                        <?php endif; ?>
                     </div>
                 </nav>
             </header>
@@ -61,24 +43,21 @@
             <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
         </footer>
     </div>
-    <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script>
-
         $(document).ready(function(){
             $(".message").each(function(){
                 if($(this).text().length > 0){
                     $(this).slideDown(500, function(){
                         $(this).delay(3000).slideUp(500)
-                    })
+                    });
                 }
-            })
+            });
+
             $(".delete-btn").on("click", function(){
-                return confirm("Etes-vous sûr de vouloir supprimer?")
-            })
+                return confirm("Etes-vous sûr de vouloir supprimer?");
+            });
+
             tinymce.init({
                 selector: '.post',
                 menubar: false,
@@ -93,22 +72,7 @@
                 'removeformat | help',
                 content_css: '//www.tiny.cloud/css/codepen.min.css'
             });
-        })
-
-        
-
-        /*
-        $("#ajaxbtn").on("click", function(){
-            $.get(
-                "index.php?action=ajax",
-                {
-                    nb : $("#nbajax").text()
-                },
-                function(result){
-                    $("#nbajax").html(result)
-                }
-            )
-        })*/
+        });
     </script>
 </body>
 </html>
