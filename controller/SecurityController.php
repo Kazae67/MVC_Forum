75c552c2e4c0cf3a98fd003c7b570a022ccffd68
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 20:32:20 +0200

    injecter + compact + factorisation + amélioration filter

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -9,93 +9,123 @@ use Model\Managers\UserManager;
 
 class SecurityController extends AbstractController implements ControllerInterface
 {
-    public function index()
-    {
+    private $userManager;
+
+    const SUCCESS_REGISTRATION = 'you are registered !';
+    const SUCCESS_LOGIN = 'successful connection';
+    const SUCCESS_LOGOUT = 'successful disconnection';
+    const ERROR_LOGOUT = 'disconnection failed';
+
+    // Injecter UserManager via le constructeur pour faciliter la réutilisation
+    public function __construct() {
+        $this->userManager = new UserManager();
+    }
+
+    // Renvoyer la vue pour l'inscription
+    public function index() {
         return [
             "view" => VIEW_DIR . "security/register.php"
         ];
     }
 
-    public function toLogin()
-    {
-        $userManager = new UserManager();
-
+    // Renvoyer la vue pour la connexion, avec la liste des utilisateurs
+    public function toLogin() {
+        $users = $this->userManager->findAll(["user_registration_date", "DESC"]);
         return [
             "view" => VIEW_DIR . "security/login.php",
-            "data" => [
-                "users" => $userManager->findAll(["user_registration_date", "DESC"])
-            ]
+            "data" => compact('users') 
         ];
     }
 
-    public function register()
-    {
+    // Cette méthode s'occupe de l'enregistrement d'un nouvel utilisateur
+    public function register() {
         if (!isset($_POST["submit"])) {
             $this->redirectTo('security', 'index');
         }
 
-        $userManager = new UserManager();
+        $nickName = $this->filterPost("nickName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $password = $this->filterPost("password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $passwordConfirm = $this->filterPost("passwordConfirm", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = $this->filterPost("email", FILTER_SANITIZE_EMAIL);
 
-        $nickName = filter_input(INPUT_POST, "nickName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $passwordConfirm = filter_input(INPUT_POST, "passwordConfirm", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-
-        if (!$nickName || !$password || !$passwordConfirm || !$email) {
-            $this->redirectTo('security', 'index');
-        }
-
-        if ($userManager->findOneByEmail($email) || $userManager->findOneByNickname($nickName)) {
-            $this->redirectTo('security', 'index');
-        }
-
-        if ($password != $passwordConfirm) {
+        if (!$this->isValidRegistration($nickName, $password, $passwordConfirm, $email)) {
             $this->redirectTo('security', 'index');
         }
 
         $password = password_hash($password, PASSWORD_DEFAULT);
-        $userManager->add(["nickName" => $nickName, "password" => $password, "email" => $email]);
+        $this->userManager->add(["nickName" => $nickName, "password" => $password, "email" => $email]);
 
-        Session::addFlash('success', 'you are registered !');
+        Session::addFlash('success', self::SUCCESS_REGISTRATION);
         $this->redirectTo('security', 'login');
     }
-    
-    public function login()
-    {
+
+    // Cette méthode s'occupe de la connexion d'un utilisateur
+    public function login() {
         if (!isset($_POST["submit"])) {
             $this->redirectTo('security', 'toLogin');
         }
 
-        $userManager = new UserManager();
+        $email = $this->filterPost("email", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $password = $this->filterPost("password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-        if (!$email || !$password) {
+        if (!$this->isValidLogin($email, $password)) {
             $this->redirectTo('security', 'toLogin');
         }
 
-        $user = $userManager->findOneByEmail($email);
+        $user = $this->userManager->findOneByEmail($email);
 
-        if (!$user || $user->getBan() == 1 || !password_verify($password, $user->getPassword())) {
+        if (!$this->isValidUser($user, $password)) {
             $this->redirectTo('security', 'toLogin');
         }
 
         Session::setUser($user);
-        Session::addFlash('success', 'sucessful connection');
+        Session::addFlash('success', self::SUCCESS_LOGIN);
         $this->redirectTo('security', 'viewProfile');
     }
 
-    
-    public function logout()
-    {
+    // Cette méthode s'occupe de la déconnexion d'un utilisateur
+    public function logout() {
         if (!$_SESSION["user"]) {
-            Session::addFlash('error', 'disconnection failed');
+            Session::addFlash('error', self::ERROR_LOGOUT);
             $this->redirectTo('user', 'index');
         }
 
         session_destroy();
+        Session::addFlash('success', self::SUCCESS_LOGOUT);
         $this->redirectTo('security', 'toLogin');
-        Session::addFlash('success', 'sucessful disconnection');
+    }
+
+    private function filterPost($key, $filter) {
+        return filter_input(INPUT_POST, $key, $filter);
+    }
+
+    private function isValidRegistration($nickName, $password, $passwordConfirm, $email) {
+        if (!$nickName || !$password || !$passwordConfirm || !$email) {
+            return false;
+        }
+
+        if ($this->userManager->findOneByEmail($email) || $this->userManager->findOneByNickname($nickName)) {
+            return false;
+        }
+
+        if ($password != $passwordConfirm) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isValidLogin($email, $password) {
+        if (!$email || !$password) {
+            return false;
+        }
+        return true;
+    }
+
+    private function isValidUser($user, $password) {
+        if (!$user || $user->getBan() == 1 || !password_verify($password, $user->getPassword())) {
+            return false;
+        }
+        return true;
     }
 }
