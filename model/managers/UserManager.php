432fbfdccaa99ecd391f8b03b1a594a64d5b8d3d
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 22:56:49 +0200

    amélioration lisibilité  + compréhension

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -1,44 +1,52 @@
 <?php
-    namespace Model\Managers;
-    
-    use App\Manager;
-    use App\DAO;
-    use Controller\ForumController;
 
-    class UserManager extends Manager{
+namespace Model\Managers;
 
-        protected $className = "Model\Entities\User";
-        protected $tableName = "user";
+use App\Manager;
+use App\DAO;
 
+/*
+La classe UserManager est utilisée pour gérer les utilisateurs. Elle hérite de la classe Manager qui fournit la connexion à la base de données.
+*/
+class UserManager extends Manager
+{
+    // Nom de la classe correspondante et de la table dans la base de données
+    protected $className = "Model\Entities\User";
+    protected $tableName = "user";
 
-        public function __construct(){
-            parent::connect();
-        }
-        public function findOneByEmail($email){
-
-            $sql = "SELECT *
-                    FROM ".$this->tableName." a
-                    WHERE a.email = :email;
-                    ";
-
-            return $this->getOneOrNullResult(
-                DAO::select($sql, ['email' => $email], false), 
-                $this->className
-            );
-        }
-        public function findOneByNickname($nickName){
-
-            $sql = "SELECT *
-                    FROM ".$this->tableName." a
-                    WHERE a.nickName = :nickName;
-                    ";
-
-            return $this->getOneOrNullResult(
-                DAO::select($sql, ['nickName' => $nickName], false), 
-                $this->className
-            );
-        }
-        
-
+    // Méthode constructeur qui établit la connexion à la base de données
+    public function __construct()
+    {
+        parent::connect();
     }
-    ?>
+
+    // Méthode pour trouver un utilisateur par son email
+    public function findOneByEmail($email)
+    {
+        // Requête SQL pour obtenir l'utilisateur avec l'email spécifié
+        $sql = "SELECT *
+                FROM " . $this->tableName . "
+                WHERE email = :email";
+
+        // Exécute la requête et renvoie le résultat sous forme d'une instance de la classe User, ou null si aucun utilisateur n'a été trouvé
+        return $this->getOneOrNullResult(
+            DAO::select($sql, ['email' => $email], false),
+            $this->className
+        );
+    }
+
+    // Méthode pour trouver un utilisateur par son pseudo
+    public function findOneByNickname($nickname)
+    {
+        // Requête SQL pour obtenir l'utilisateur avec le pseudo spécifié
+        $sql = "SELECT *
+                FROM " . $this->tableName . "
+                WHERE nickname = :nickname";
+
+        // Exécute la requête et renvoie le résultat sous forme d'une instance de la classe User, ou null si aucun utilisateur n'a été trouvé
+        return $this->getOneOrNullResult(
+            DAO::select($sql, ['nickname' => $nickname], false),
+            $this->className
+        );
+    }
+}
