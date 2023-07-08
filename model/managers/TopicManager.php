d2788a4d44ae572414f4a5e9f8be52b980d28ae4
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 22:50:11 +0200

    amélioration de clarté, quelques modifications

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -5,31 +5,34 @@ namespace Model\Managers;
 use App\Manager;
 use App\DAO;
 
+/*
+La classe TopicManager est utilisée pour la gestion des sujets (topics). Elle hérite de la classe Manager qui fournit la connexion à la base de données.
+*/
 class TopicManager extends Manager
 {
-    protected $tableName = "topic";
+    // Nom de la classe correspondante et de la table dans la base de données
     protected $className = "Model\Entities\Topic";
+    protected $tableName = "topic";
 
+    // Méthode constructeur qui établit la connexion à la base de données
     public function __construct()
     {
         parent::connect();
     }
 
-    // recherche et récupère tous les topics correspondant à une catégorie spécifiée par l'ID
-    public function findTopicsByCategory($id)
+    // Méthode pour obtenir tous les sujets (topics) associés à une catégorie spécifique
+    public function findTopicsByCategory($categoryId)
     {
-        $sql = "
-            SELECT *
-            FROM {$this->tableName} table
-            WHERE table.category_id = :category_id
-            ORDER BY creation_date DESC
-        ";
+        // Requête SQL pour obtenir tous les sujets associés à une catégorie spécifique, triés par la date de création
+        $sql = "SELECT *
+                FROM " . $this->tableName . "
+                WHERE category_id = :categoryId
+                ORDER BY creation_date DESC";
 
-        // Les résultats de la requête sont ensuite traités à l'aide de la méthode getMultipleResults() de la classe Manager, qui convertit les résultats en instances de la classe $this->className (ici, Model\Entities\Topic).
+        // Exécute la requête et renvoie le résultat sous forme de multiples instances de la classe Topic, ou un tableau vide si aucun sujet n'a été trouvé
         return $this->getMultipleResults(
-            DAO::select($sql, ['category_id' => $id]),
+            DAO::select($sql, ['categoryId' => $categoryId], false),
             $this->className
         );
     }
-
 }
