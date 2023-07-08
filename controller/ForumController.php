8fb7742404d90f9f4cd31d7f6e836b9113dfe584
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 20:18:18 +0200

    ajout d'injection de dépendance, + compact pour le passage des données

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -1,29 +1,36 @@
 <?php
 
-    namespace Controller;
+namespace Controller;
 
-    use App\Session;
-    use App\AbstractController;
-    use App\ControllerInterface;
-    use Model\Managers\TopicManager;
-    use Model\Managers\PostManager;
-    
-    class ForumController extends AbstractController implements ControllerInterface{
+use App\Session;
+use App\AbstractController;
+use App\ControllerInterface;
+use Model\Managers\TopicManager;
 
-        public function index(){
-          
+class ForumController extends AbstractController implements ControllerInterface {
 
-           $topicManager = new TopicManager(); 
+    private $topicManager;
 
-            return [
-                "view" => VIEW_DIR."forum/listTopics.php", 
-                "data" => [ 
-                    "topics" => $topicManager->findAll(["topic_creation_date", "DESC"]) // (4)
-                ]
-            ];
-        
-        }
-
-        // Autres function de redirection
-
+    // Constructeur pour l'injection de dépendance de TopicManager
+    public function __construct() {
+        // Initialisation de TopicManager
+        $this->topicManager = new TopicManager();
     }
+
+    // Méthode pour afficher la liste des sujets du forum
+    public function index() {
+        // Récupération des sujets depuis la base de données
+        $topics = $this->topicManager->findAll(["topic_creation_date", "DESC"]);
+
+        // Retourne la vue à afficher et les données à y injecter
+        return [
+            // Chemin vers le fichier de vue
+            "view" => VIEW_DIR . "forum/listTopics.php",
+            // Les données à passer à la vue (dans ce cas, les sujets)
+            "data" => compact('topics')
+        ];
+    }
+
+    // Autres fonctions de redirection
+
+}
