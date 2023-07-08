a3c1ce969f76aaf027558f2152103a8bc6c70f8c
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 22:44:25 +0200

    parents optimisé en une fois

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -4,32 +4,34 @@ namespace Model\Managers;
 
 use App\Manager;
 use App\DAO;
+
+/*
+La classe PostManager est dédiée à la gestion des posts. Elle hérite de la classe Manager qui permet une connexion à la base de données.
+*/
 class PostManager extends Manager
 {
-
+    // Nom de la classe correspondante et de la table dans la base de données
     protected $className = "Model\Entities\Post";
     protected $tableName = "post";
 
-
+    // Méthode constructeur qui se connecte à la base de données
     public function __construct()
     {
         parent::connect();
     }
 
-    public function findPostByTopic($id)
+    // Méthode pour trouver tous les posts associés à un topic spécifique
+    public function findPostByTopic($topicId)
     {
-
-        parent::connect();
-
+        // Requête SQL pour obtenir tous les posts associés à un topic spécifique
         $sql = "SELECT *
-                FROM " . $this->tableName . " a
-                WHERE a.topic_id = :id
-                ";
+                FROM " . $this->tableName . "
+                WHERE topic_id = :topicId";
 
+        // Exécute la requête et renvoie le résultat sous forme de multiples instances de la classe Post, ou un tableau vide si aucun post n'a été trouvé
         return $this->getMultipleResults(
-            DAO::select($sql, ['id' => $id], true),
+            DAO::select($sql, ['topicId' => $topicId], true),
             $this->className
         );
     }
-
 }
