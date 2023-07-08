f4752daac06dfb0f2642e0a3ed2f14f388dea3cb
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 20:42:32 +0200

    ajout injecter + compact + commentaires

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -10,34 +10,46 @@ use Model\Managers\TopicManager;
 
 class TopicController extends AbstractController implements ControllerInterface
 {
+    private $categoryManager;
+    private $topicManager;
+
+    // Injecter CategoryManager et TopicManager via le constructeur pour la réutilisation
+    public function __construct()
+    {
+        $this->categoryManager = new CategoryManager();
+        $this->topicManager = new TopicManager();
+    }
+
+    // La méthode index pour lister toutes les catégories
     public function index()
     {
-        $categoryManager = new CategoryManager();
+        $categories = $this->categoryManager->findAll(["categoryLabel", "DESC"]);
 
+        // Les données sont extraites et renvoyées sous forme d'un tableau associatif avec les clés 'view' et 'data'.
         return [
             "view" => VIEW_DIR . "forum/listCategories.php",
-            "data" => [
-                "categories" => $categoryManager->findAll(["categoryLabel", "DESC"])
-            ]
+            "data" => compact('categories') // Utilisation de compact() ici
         ];
     }
 
+    // Méthode pour lister les sujets par catégorie
     public function listTopicsByCategory($id)
     {
-        $topicManager = new TopicManager();
-        $categoryManager = new CategoryManager();
-        $category = $categoryManager->findOneById($id);
+        // Recherche de la catégorie par son ID
+        $category = $this->categoryManager->findOneById($id);
 
+        // Si la catégorie n'existe pas, on redirige vers l'index des catégories
         if (!$category) {
             $this->redirectTo('category', 'index');
         }
 
+        $topics = $this->topicManager->findTopicsByCategory($id);
+
+        // Les données sont extraites et renvoyées sous forme d'un tableau associatif avec les clés 'view' et 'data'.
+        // Les données incluent les sujets trouvés par la catégorie et la catégorie elle-même.
         return [
             "view" => VIEW_DIR . "forum/listTopics.php",
-            "data" => [
-                "topics" => $topicManager->findTopicsByCategory($id),
-                "category" => $category,
-            ]
+            "data" => compact('topics', 'category') // Utilisation de compact() ici
         ];
     }
 
