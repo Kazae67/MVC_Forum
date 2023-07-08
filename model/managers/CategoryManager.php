0f875491c54f135454d55c079effc5833c29f8ab
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 22:37:28 +0200

    commentaires + l'appel du parent une seule fois

--- model/managers/CategoryManager.php
+++ model/managers/CategoryManager.php
@@ -1,46 +1,40 @@
 <?php
-/* 
-Il y a 1 manager par "entities", ce n'est pas comme le controller qui regroupe des actions.
-Les managers sont aussi des classes.
-C'est la que je vais mettre mes requêtes SQL au serveur,
-qui seront appelés par la suite par le CONTROLLER.
+/*
+Un Manager est dédié à chaque "entité", contrairement au Controller qui regroupe des actions.
+Les Managers sont également des classes.
+C'est ici que nous écrivons nos requêtes SQL,
+qui seront appelées par la suite par le CONTROLLER.
 */
-    namespace Model\Managers;
-    
-    use App\Manager;
-    use App\DAO;
-    use Controller\ForumController;
+namespace Model\Managers;
+
+use App\Manager;
+use App\DAO;
 
 class CategoryManager extends Manager
 {
     protected $className = "Model\Entities\Category";
     protected $tableName = "category";
 
-
+    // La méthode constructeur se connecte à la base de données
     public function __construct()
     {
         parent::connect();
     }
 
+    // Méthode pour trouver une catégorie par son ID
     public function findOneById($id)
     {
-        parent::connect();
-
+        // Requête SQL pour obtenir une catégorie par son ID
         $sql = "SELECT *
-                    FROM ".$this->tableName." a
-                    WHERE a.id_".$this->tableName." = :id
+                    FROM ".$this->tableName."
+                    WHERE id_".$this->tableName." = :id
                     ";
 
+        // Exécute la requête et renvoie un seul résultat ou null si aucune catégorie n'a été trouvée
         return $this->getOneOrNullResult(
             DAO::select($sql, ['id' => $id], false),
             $this->className
         );
     }
 }
-    //     public function createNewCategory(){
-    //         if($_SESSION["user"]->getRole() == "admin"){
-                
-    //             //to be contineud
-    //     }
-    // }
-    ?>
+?>
