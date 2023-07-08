766c05eeb804b28fc33f7b298d751d9a1a1d6a8c
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-08 20:21:26 +0200

    injection de dépendance + compact + commentaires

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -6,20 +6,31 @@ use App\ControllerInterface;
 use Model\Managers\UserManager;
 
 class HomeController extends AbstractController implements ControllerInterface {
+    private $userManager;
+
+    // Constructeur pour l'injection de dépendance de UserManager
+    public function __construct() {
+        $this->userManager = new UserManager();
+    }
+
+    // Méthode pour la page d'accueil
     public function index() {
         return [
             "view" => VIEW_DIR . "home.php"
         ];
     }
 
+    // Méthode pour afficher la liste des utilisateurs
     public function users() {
-        $this->restrictTo("role");
+        // Restreindre l'accès à cette méthode aux utilisateurs ayant le rôle 'admin'
+        $this->restrictTo("admin");
+
+        // Récupérer tous les utilisateurs, triés par date d'inscription décroissante
+        $users = $this->userManager->findAll(['user_registration_date', 'DESC']);
 
         return [
             "view" => VIEW_DIR . "security/users.php",
-            "data" => [
-                "users" => (new UserManager())->findAll(['user_registration_date', 'DESC'])
-            ]
+            "data" => compact('users')
         ];
     }
 }
