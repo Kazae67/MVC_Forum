a463303d50d1d592d1bcaedfa5ddc9a0f9ecfd2a
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 18:01:56 +0200

    ajout des commentaires

--- view/forum/newTopic.php
+++ view/forum/newTopic.php
@@ -1,10 +1,13 @@
 <?php
+// Vérifie si une catégorie a été envoyée dans les données de réponse et l'assigne si c'est le cas
 $category = $result["data"]['category'] ?? null;
 
+// Vérifie si un utilisateur est connecté
 $userLoggedIn = isset($_SESSION["user"]);
 ?>
 
 <?php if ($userLoggedIn && $category) : ?>
+    <!-- NOUVEAU TOPIC -->
     <h3>Add a topic to the category "<?= $category->getCategoryLabel() ?>"</h3>
 
     <form class="form-new-topic" action="index.php?ctrl=topic&action=newTopic&id=<?= $category->getId() ?>" method="POST">
@@ -16,9 +19,10 @@ $userLoggedIn = isset($_SESSION["user"]);
 
         <input class="button" type="submit" name="submit" id="submit" value="Create the topic">
     </form>
-
 <?php elseif(!$userLoggedIn) : ?>
+    <!-- L'utilisateur n'est pas connecté -->
     <a href="index.php?ctrl=security&action=login">Please log in</a>
 <?php else : ?>
+    <!-- Pas de catégorie sélectionnée -->
     <p>Please select a category first</p>
 <?php endif; ?>
