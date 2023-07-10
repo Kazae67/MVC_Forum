e3786e1a305eb91db8e3008bf34fa72ed13a9563
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 17:46:27 +0200

    ajout de commentaire + légère amélioration du code

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,19 +1,26 @@
 <?php
-$error = $result["data"]['error'] ?? null;
-$categories = $result["data"]['categories'];
-$admin = isset($_SESSION["user"]) && $_SESSION["user"]->getRole() == 'admin';
+    // Vérifier si il y a des erreurs dans la réponse
+    $error = $result["data"]['error'] ?? null;
+
+    // Récupérer les catégories de la réponse
+    $categories = $result["data"]['categories'];
+
+    // Vérifier si l'utilisateur actuel est un administrateur
+    $admin = isset($_SESSION["user"]) && $_SESSION["user"]->getRole() == 'admin';
 ?>
 
 <h3>List of categories</h3>
 
+<!-- Si l'utilisateur est un administrateur, afficher le formulaire d'ajout de catégorie -->
 <?php if ($admin) : ?>
     <form action="index.php?ctrl=category&action=addCategory" method="post">
         <label for="categoryLabel">New category :</label>
         <input type="text" name="categoryLabel" id="categoryLabel">
-        <input type="submit" name="submit" id="submit" value="add">
+        <input type="submit" name="submit" id="submit" value="Add">
     </form>
 <?php endif; ?>
 
+<!-- Si il y a des catégories, afficher la liste des catégories -->
 <?php if ($categories) : ?>
     <table class="list-categories">
         <thead>
@@ -22,6 +29,7 @@ $admin = isset($_SESSION["user"]) && $_SESSION["user"]->getRole() == 'admin';
             </tr>
         </thead>
         <tbody>
+            <!-- Parcourir chaque catégorie et afficher son nom -->
             <?php foreach ($categories as $category) : ?>
                 <tr>
                     <td>
