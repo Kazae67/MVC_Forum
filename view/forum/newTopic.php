4d2d9b4ccda46aa34e4a8aad809d020ec60e4b31
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 17:57:42 +0200

    ajout de condition  avec redirection pour qu'il se co

--- view/forum/newTopic.php
+++ view/forum/newTopic.php
@@ -1,9 +1,10 @@
 <?php
 $category = $result["data"]['category'] ?? null;
+
 $userLoggedIn = isset($_SESSION["user"]);
 ?>
 
-<!-- NEW TOPIC -->
+<?php if ($userLoggedIn && $category) : ?>
     <h3>Add a topic to the category "<?= $category->getCategoryLabel() ?>"</h3>
 
     <form class="form-new-topic" action="index.php?ctrl=topic&action=newTopic&id=<?= $category->getId() ?>" method="POST">
@@ -13,10 +14,11 @@ $userLoggedIn = isset($_SESSION["user"]);
         <label for="text">Topic description</label>
         <textarea rows="5" name="text" id="text"></textarea>
 
-        <input class="button" type="submit" name="submit" id="submit" value="Créer le topic">
+        <input class="button" type="submit" name="submit" id="submit" value="Create the topic">
     </form>
 
-<?php else : ?>
+<?php elseif(!$userLoggedIn) : ?>
     <a href="index.php?ctrl=security&action=login">Please log in</a>
+<?php else : ?>
+    <p>Please select a category first</p>
 <?php endif; ?>
-
