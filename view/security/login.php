f51f3ecf657ca3ed17cf8c04fd9d2051794d14c5
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 19:12:58 +0200

    ajout de commentaires sur login.php

--- view/security/login.php
+++ view/security/login.php
@@ -1,11 +1,15 @@
+<!-- FORMULAIRE DE CONNEXION -->
 <h3>Login</h3>
 <form action="index.php?ctrl=security&action=login" method="post">
-
+    
+    <!-- Champ d'entrée pour l'e-mail -->
     <label for="email">Email</label>
-    <input type ="text" name="email" id="email" required></input>
+    <input type="text" name="email" id="email" required />
 
+    <!-- Champ d'entrée pour le mot de passe -->
     <label for="password">Password</label>
-    <input type ="password" name="password" id="password" required></input>
+    <input type="password" name="password" id="password" required />
 
-    <input class="button" type="submit" name="submit" id="submit" value="login">
+    <!-- Bouton de soumission du formulaire -->
+    <input class="button" type="submit" name="submit" id="submit" value="Login" />
 </form>
