4ebc204eb527b48c942254c2d40157d380c6b2e9
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 19:14:05 +0200

    ajout de commentaires register.php

--- view/security/register.php
+++ view/security/register.php
@@ -1,20 +1,23 @@
+<!-- FORMULAIRE D'INSCRIPTION -->
 <h3>Register</h3>
-
-<!--REGISTER-->
-<form action="index.php?ctrl=security&action=register" method="POST">
-
+<form action="index.php?ctrl=security&action=register" method="post">
+    
+    <!-- Champ d'entrée pour le pseudonyme -->
     <label for="nickName">Nickname</label>
-    <input type="text" name="nickName" id="nickName" required>
+    <input type="text" name="nickName" id="nickName" required />
 
+    <!-- Champ d'entrée pour l'e-mail -->
     <label for="email">Email</label>
-    <input type ="text" name="email" id="email" required></input>
+    <input type="text" name="email" id="email" required />
 
+    <!-- Champ d'entrée pour le mot de passe -->
     <label for="password">Password</label>
-    <input type ="password" name="password" id="password" required></input>
+    <input type="password" name="password" id="password" required />
 
-    <label for="passwordConfirm">Password</label>
-    <input type ="password" name="password_confirmation" id="password_confirmation" required></input>
+    <!-- Champ d'entrée pour la confirmation du mot de passe -->
+    <label for="password_confirmation">Confirm Password</label>
+    <input type="password" name="password_confirmation" id="password_confirmation" required />
 
-    <input class="button" type="submit" name="submit" id="submit" value="register">
-
+    <!-- Bouton de soumission du formulaire -->
+    <input class="button" type="submit" name="submit" id="submit" value="Register" />
 </form>
