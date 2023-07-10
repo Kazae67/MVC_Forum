e5fb56813dfd0dd178e139d60284a3359b9d9a7c
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-10 18:30:03 +0200

    ajout de commentaires

--- view/forum/infoCategory.php
+++ view/forum/infoCategory.php
@@ -1,19 +1,21 @@
 <?php
+// On assigne les catégories ou une liste vide si aucune catégorie n'est présente
 $categories = $result["data"]['categories'] ?? [];
 ?>
 
-<!-- list -->
+<!-- LISTE DES CATÉGORIES -->
 <h3>Category list</h3>
 
-    <form action="index.php?ctrl=category&action=addNewCategory" method="post">
-        <label for="categoryLabel">New Category :</label>
-        <input type="text" name="categoryLabel" id="categoryLabel">
-        <input type="submit" name="submit" id="submit" value="new">
-    </form>
+<form action="index.php?ctrl=category&action=addNewCategory" method="post">
+    <label for="categoryLabel">New Category:</label>
+    <input type="text" name="categoryLabel" id="categoryLabel">
+    <input type="submit" name="submit" id="submit" value="Add">
+</form>
 
-    <?php foreach ($categories as $category) : ?>
+<?php foreach ($categories as $category) : ?>
     <div class="category-post">
         <p>
+            <!-- Le lien dirige vers la liste des sujets de la catégorie -->
             <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?= $category->getTitle() ?>">
                 <?= $category->getCategoryLabel() ?>
             </a>
