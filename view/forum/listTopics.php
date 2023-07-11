fbcb99876cc937f4dec5fee6bc74ed428dc76875
Stagiaire Elan <ammrkouzeha@gmail.com>
2023-07-11 11:16:53 +0200

    condition & informations sur l'user, + si il est admin, locked etc

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -10,3 +10,44 @@ $admin = isset($_SESSION["user"]) && in_array($_SESSION["user"]->getRole(), ["ad
   <h3>Category: <?= $category->getCategoryLabel() ?></h3>
   <a title="Create a new topic" class="button" href="index.php?ctrl=topic&action=linkAddTopic&id=<?= $category->getId() ?>">NEW TOPIC</a>
 </div>
+
+<?php if ($topics): ?>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Creation date</th>
+        <?php if ($admin): ?>
+          <th>Admin</th>
+        <?php endif; ?>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($topics as $topic): ?>
+        <tr>
+          <td>
+            <a title="View topic" href="index.php?ctrl=post&action=listPostByTopic&id=<?= $topic->getId() ?>">
+              <?= $topic->getTitle() ?>
+            </a>
+          </td>
+          <td>
+            <p title="View profile" class="<?= $topic->getUser()->getRole()?>">
+              <a href="index.php?ctrl=security&action=viewUsersProfiles&id=<?= $topic->getUser()->getId() ?>">
+                <?= $topic->getUser()->getNickName() ?>
+              </a>
+            </p>
+          </td>
+          <td>
+            <div class="container-admin">
+              <div title="<?= $topic->getIs_Locked() ? "Topic locked" : "Topic closed" ?>">
+                <i class="fa-solid fa-lock<?= $topic->getIs_Locked() ? "" : "-open" ?>"></i>
+              </div>
+            </div>
+          </td>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
