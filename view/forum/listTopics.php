0c3b64e52d6198d63af7d11d7963abb2f35e9eaf
Stagiaire Elan <ammrkouzeha@gmail.com>
2023-07-11 11:09:00 +0200

    structuration listTopics.php

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,17 +1,12 @@
 <?php
 $topics = $result["data"]["topics"] ?? null;
 $category = $result["data"]["category"] ?? null;
+
+$admin = isset($_SESSION["user"]) && in_array($_SESSION["user"]->getRole(), ["admin", "moderator"]);
 ?>
 
-<h1>Topics list</h1>
-
-<?php
-if ($topics !== null) {
-    foreach ($topics as $topic) {
-        ?>
-        <p><?= $topic->getTitle() ?></p>
-        <?php
-    }
-}
-?>
-
+<div class="header-topic">
+  <a title="Go back to categories" href="index.php?ctrl=category" class="button">Back to category list</a>
+  <h3>Category: <?= $category->getCategoryLabel() ?></h3>
+  <a title="Create a new topic" class="button" href="index.php?ctrl=topic&action=linkAddTopic&id=<?= $category->getId() ?>">NEW TOPIC</a>
+</div>
