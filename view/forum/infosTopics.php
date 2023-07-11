dcba2ef4729e5b3010ecb2a3e705dcb37e7bc690
Stagiaire Elan <ammrkouzeha@gmail.com>
2023-07-11 09:33:07 +0200

    ajout de structuration infosTopics.php

--- view/forum/infosTopics.php
+++ view/forum/infosTopics.php
@@ -1,4 +1,12 @@
 <?php
-
-
+$topics = $result["data"]["topics"] ?? null;
+$category = $result["data"]["category"] ?? null;
 ?>
+
+<div class="header-infos-topics">
+  <a href="index.php?ctrl=category" class="button">Back to the category list</a>
+  <h3>Category: <?= $category->getCategoryLabel() ?></h3>
+  <a class="button" href="index.php?ctrl=topic&action=addTopic&id=<?= $category->getId() ?>">NEW TOPIC</a>
+</div>
+
+
