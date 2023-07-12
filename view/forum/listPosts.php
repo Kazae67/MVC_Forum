29a6c5c37d4651ea0269c47bf9cec524598aed43
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-12 19:47:42 +0200

    structuration + redif

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -2,3 +2,10 @@
 $posts = $result["data"]['posts'] ?? null;
 $topic = $result["data"]['topic'] ?? null;
 ?>
+
+<div class="post-container">
+  <div class="post-header">
+      <h2 class="post-category"><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$topic->getCategory()->getId()?>"><?=$topic->getCategory()->getCategoryLabel()?></a></h2>
+      <h1 class="post-title">/<?=$topic->getTitle()?></h1>
+    </div>
+</div>
