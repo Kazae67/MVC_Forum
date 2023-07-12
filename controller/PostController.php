7837b18b7e1552a9519d4c467107833b340bc9fa
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-12 18:58:14 +0200

    lister les posts par topic

--- controller/PostController.php
+++ controller/PostController.php
@@ -7,6 +7,7 @@ use Model\Managers\PostManager;
 use Model\Managers\TopicManager;
 
 class PostController extends AbstractController implements ControllerInterface {
+
     private $postManager;
     private $topicManager;
 
@@ -21,7 +22,13 @@ class PostController extends AbstractController implements ControllerInterface {
         return $this->render("forum/listPosts.php", ["posts" => $posts]);
     }
 
-    // Méthode générique pour afficher une vue avec ses données
+    public function listPostByTopic($topicId) {
+        $posts = $this->postManager->findPostByTopic($topicId);
+        $topic = $this->topicManager->findOneById($topicId);
+        return $this->render("forum/listPosts.php", ["posts" => $posts, "topic" => $topic]);
+    }
+
+    // Méthode générique pour afficher une vue avec des données
     private function render($view, $data) {
         return [
             "view" => VIEW_DIR . $view,
