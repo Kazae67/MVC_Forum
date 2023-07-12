0d827187291c29aab2b206196b16d740093ee82d
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-12 18:48:09 +0200

    initialisation des managers

--- controller/PostController.php
+++ controller/PostController.php
@@ -3,7 +3,18 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use Model\Managers\PostManager;
+use Model\Managers\TopicManager;
 
 class PostController extends AbstractController implements ControllerInterface {
+
+    private $postManager;
+    private $topicManager;
+
+    public function __construct()
+    {
+        $this->postManager = new PostManager();
+        $this->topicManager = new TopicManager();
+    }
 
 }
