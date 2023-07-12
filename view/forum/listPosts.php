31979a270d7e6a031974260fa798946647130249
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-12 19:51:32 +0200

    si l'utilisateur existe dans la session et role admin moderateur

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -1,11 +1,15 @@
 <?php
 $posts = $result["data"]['posts'] ?? null;
 $topic = $result["data"]['topic'] ?? null;
-?>
 
+if (isset($_SESSION["user"]) && in_array($_SESSION["user"]->getRole(), ['admin', 'moderator'])) {
+    $admin = true;
+}
+?>
 <div class="post-container">
   <div class="post-header">
       <h2 class="post-category"><a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$topic->getCategory()->getId()?>"><?=$topic->getCategory()->getCategoryLabel()?></a></h2>
       <h1 class="post-title">/<?=$topic->getTitle()?></h1>
     </div>
 </div>
+
