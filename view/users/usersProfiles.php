6e76d6b6e9a843f693b26582d8dfb232d158129f
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-14 22:31:06 +0200

    userbaned

--- view/users/usersProfiles.php
+++ view/users/usersProfiles.php
@@ -8,6 +8,7 @@ $lastPosts = isset($result["data"]['posts']) ? $result["data"]['posts'] : null;
 $admin = isset($_SESSION['user']) && in_array($_SESSION['user']->getRole(), ['admin', 'administrator']); // Vérifie si la clé 'user' existe dans $_SESSION et si son rôle correspond à 'admin' ou 'administrator'. Stocke le résultat dans $admin
 
 
+$userBanned = $user && $user->getBan() == 1; 
 
 ?>
 
