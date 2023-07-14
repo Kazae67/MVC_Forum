9fcd01a8eef137244469c3ae74827777af1a680f
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-14 22:30:36 +0200

    si user exist dans session condition

---
+++ view/users/usersProfiles.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = isset($result["data"]['user']) ? $result["data"]['user'] : null; 
+
+
+$lastPosts = isset($result["data"]['posts']) ? $result["data"]['posts'] : null; 
+
+$admin = isset($_SESSION['user']) && in_array($_SESSION['user']->getRole(), ['admin', 'administrator']); // Vérifie si la clé 'user' existe dans $_SESSION et si son rôle correspond à 'admin' ou 'administrator'. Stocke le résultat dans $admin
+
+
+
+?>
+
+
+
