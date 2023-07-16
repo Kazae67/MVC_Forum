68d8836d988e25f157550a977fecb7923a8bd4ce
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-16 22:03:23 +0200

    banuserbyid

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -5,8 +5,9 @@ namespace Model\Managers;
 use App\Manager;
 use App\DAO;
 
-
-// La classe UserManager est utilisée pour gérer les utilisateurs. Elle hérite de la classe Manager qui fournit la connexion à la base de données.
+/*
+La classe UserManager est utilisée pour gérer les utilisateurs. Elle hérite de la classe Manager qui fournit la connexion à la base de données.
+*/
 class UserManager extends Manager
 {
     // Nom de la classe correspondante et de la table dans la base de données
@@ -47,6 +48,15 @@ class UserManager extends Manager
             DAO::select($sql, ['nickname' => $nickname], false),
             $this->className
         );
+    }
+
+    public function banUserById($id)
+    {
+        $sql =  "UPDATE " . $this->tableName .
+            " SET ban = 1 
+             WHERE id_user = :id";
+
+        return DAO::update($sql, ['id' => $id]);
     }
     
 }
