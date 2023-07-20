f8699946f4580a9b3b514e5695c9312e941d0bac
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-20 11:37:33 +0200

    sauvegarde

--- view/security/usersProfiles.php
+++ view/security/usersProfiles.php
@@ -5,7 +5,8 @@ $user = isset($result["data"]['user']) ? $result["data"]['user'] : null;
 // Vérifie si la clé "posts" existe dans $result["data"] et stocke sa valeur dans $lastPosts
 $lastPosts = isset($result["data"]['posts']) ? $result["data"]['posts'] : null; 
 
-$admin = isset($_SESSION['user']) && in_array($_SESSION['user']->getRole(), ['admin', 'administrator']); // Vérifie si la clé 'user' existe dans $_SESSION et si son rôle correspond à 'admin' ou 'administrator'. Stocke le résultat dans $admin
+// Vérifie si la clé 'user' existe dans $_SESSION et si son rôle correspond à 'admin' ou 'administrator'. Stocke le résultat dans $admin
+$admin = isset($_SESSION['user']) && in_array($_SESSION['user']->getRole(), ['admin', 'administrator']);
 
 // Vérifie si l'utilisateur est banni
 $userBanned = $user && $user->getBan() == 1; 
