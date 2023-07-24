8e7ff4b47f21e710bd129ba4280f289716dac814
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-24 16:41:14 +0200

    switch tout les commentaires en français

--- model/entities/User.php
+++ model/entities/User.php
@@ -29,11 +29,19 @@ final class User extends Entity
         }
     }
 
+    /**
+     * Récupérer la valeur de id
+     */ 
     public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * Définir la valeur de id
+     *
+     * @return  self
+     */ 
     public function setId_user(int $id): self
     {
         $this->id = $id;
@@ -41,11 +49,17 @@ final class User extends Entity
         return $this;
     }
 
+    /**
+     * Récupérer la valeur de nickName
+     */ 
     public function getNickName(): string
     {
         return $this->nickName;
     }
 
+    /**
+     * Définir la valeur de nickName
+     */ 
     public function setNickName(string $nickName)
     {
         $this->nickName = $nickName;
@@ -53,11 +67,17 @@ final class User extends Entity
         return $this;
     }
 
+    /**
+     * Récupérer la valeur de password
+     */ 
     public function getPassword(): string
     {
         return $this->password;
     }
 
+    /**
+     * Définir la valeur de password
+     */ 
     public function setPassword(string $password)
     {
         $this->password = $password;
@@ -65,11 +85,17 @@ final class User extends Entity
         return $this;
     }
 
+    /**
+     * Récupérer la valeur de email
+     */ 
     public function getEmail(): string
     {
         return $this->email;
     }
 
+    /**
+     * Définir la valeur de email
+     */ 
     public function setEmail(string $email)
     {
         $this->email = $email;
@@ -77,11 +103,17 @@ final class User extends Entity
         return $this;
     }
 
+    /**
+     * Récupérer la valeur de user_registration_date
+     */ 
     public function getUser_registration_date()
     {
         return $this->user_registration_date;
     }
 
+    /**
+     * Définir la valeur de user_registration_date
+     */ 
     public function setUser_registration_date($user_registration_date)
     {
         $this->user_registration_date = $user_registration_date;
@@ -89,11 +121,19 @@ final class User extends Entity
         return $this;
     }
 
+    /**
+     * Récupérer la valeur de role
+     */ 
     public function getRole()
     {
         return $this->role;
     }
 
+    /**
+     * Définir la valeur de role
+     *
+     * @return  self
+     */ 
     public function setRole($role)
     {
         $this->role = $role;
@@ -106,18 +146,26 @@ final class User extends Entity
      *
      * @param string $role Le rôle à vérifier
      * 
-     * @return bool Vrai si l'utilisateur a le rôle, sinon, faux
+     * @return bool True si l'utilisateur a le rôle, False sinon
      */
     public function hasRole(string $role): bool
     {
         return $this->role === $role;
     }
 
+    /**
+     * Récupérer la valeur de ban
+     */ 
     public function getBan()
     {
         return $this->ban;
     }
 
+    /**
+     * Définir la valeur de ban
+     *
+     * @return  self
+     */ 
     public function setBan($ban)
     {
         $this->ban = $ban;
