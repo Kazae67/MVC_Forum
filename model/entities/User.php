251a0744b4d4207726e16d49048e67fa1bd2b9f2
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-24 16:35:46 +0200

    ajout de catégorie si admin réussi

--- model/entities/User.php
+++ model/entities/User.php
@@ -29,19 +29,11 @@ final class User extends Entity
         }
     }
 
-    /**
-     * Get the value of id
-     */ 
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
     public function setId_user(int $id): self
     {
         $this->id = $id;
@@ -49,17 +41,11 @@ final class User extends Entity
         return $this;
     }
 
-    /**
-     * Get the value of nickName
-     */ 
     public function getNickName(): string
     {
         return $this->nickName;
     }
 
-    /**
-     * Set the value of nickName
-     */ 
     public function setNickName(string $nickName)
     {
         $this->nickName = $nickName;
@@ -67,17 +53,11 @@ final class User extends Entity
         return $this;
     }
 
-    /**
-     * Get the value of password
-     */ 
     public function getPassword(): string
     {
         return $this->password;
     }
 
-    /**
-     * Set the value of password
-     */ 
     public function setPassword(string $password)
     {
         $this->password = $password;
@@ -85,17 +65,11 @@ final class User extends Entity
         return $this;
     }
 
-    /**
-     * Get the value of email
-     */ 
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * Set the value of email
-     */ 
     public function setEmail(string $email)
     {
         $this->email = $email;
@@ -103,17 +77,11 @@ final class User extends Entity
         return $this;
     }
 
-    /**
-     * Get the value of user_registration_date
-     */ 
     public function getUser_registration_date()
     {
         return $this->user_registration_date;
     }
 
-    /**
-     * Set the value of user_registration_date
-     */ 
     public function setUser_registration_date($user_registration_date)
     {
         $this->user_registration_date = $user_registration_date;
@@ -121,19 +89,11 @@ final class User extends Entity
         return $this;
     }
 
-    /**
-     * Get the value of role
-     */ 
     public function getRole()
     {
         return $this->role;
     }
 
-    /**
-     * Set the value of role
-     *
-     * @return  self
-     */ 
     public function setRole($role)
     {
         $this->role = $role;
@@ -141,19 +101,17 @@ final class User extends Entity
         return $this;
     }
 
-        /**
-     * Get the value of ban
-     */ 
+
+    public function hasRole(string $role): bool
+    {
+        return $this->role === $role;
+    }
+
     public function getBan()
     {
         return $this->ban;
     }
 
-    /**
-     * Set the value of ban
-     *
-     * @return  self
-     */ 
     public function setBan($ban)
     {
         $this->ban = $ban;
@@ -161,4 +119,5 @@ final class User extends Entity
         return $this;
     }
 }
+
 ?>
