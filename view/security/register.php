2a2e8640c1cff80f96ed4ae0fffd65f750565593
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-26 15:43:54 +0200

    ajout de form + style.css pour register

--- view/security/register.php
+++ view/security/register.php
@@ -1,23 +1,34 @@
 <!-- FORMULAIRE D'INSCRIPTION -->
-<h3>Register</h3>
-<form action="index.php?ctrl=security&action=register" method="POST">
-    
-    <!-- Champ d'entrée pour le pseudonyme -->
-    <label for="nickName">Nickname</label>
-    <input type="text" name="nickName" id="nickName" required />
+<div class="registration-container">
+  <div class="registration-form">
+    <h3>Register</h3>
+    <form action="index.php?ctrl=security&action=register" method="POST">
+      <!-- Champ d'entrée pour le pseudonyme -->
+      <div class="form-group">
+        <label for="nickName">Nickname</label>
+        <input type="text" name="nickName" id="nickName" required />
+      </div>
 
-    <!-- Champ d'entrée pour l'e-mail -->
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" required />
+      <!-- Champ d'entrée pour l'e-mail -->
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" required />
+      </div>
 
-    <!-- Champ d'entrée pour le mot de passe -->
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" required />
+      <!-- Champ d'entrée pour le mot de passe -->
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required />
+      </div>
 
-    <!-- Champ d'entrée pour la confirmation du mot de passe -->
-    <label for="password_confirmation">Confirm Password</label>
-    <input type="password" name="password_confirmation" id="password_confirmation" required />
+      <!-- Champ d'entrée pour la confirmation du mot de passe -->
+      <div class="form-group">
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" required />
+      </div>
 
-    <!-- Bouton de soumission du formulaire -->
-    <input class="button" type="submit" name="submit" id="submit" value="Register" />
-</form>
+      <!-- Bouton de soumission du formulaire -->
+      <input class="button" type="submit" name="submit" id="submit" value="Register" />
+    </form>
+  </div>
+</div>
