6cf5e825783ae0458bcacc25095cf5cdd63e765a
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-26 15:56:53 +0200

    ajout de form + style.css pour login

--- view/security/login.php
+++ view/security/login.php
@@ -1,15 +1,22 @@
 <!-- FORMULAIRE DE CONNEXION -->
-<h3>Login</h3>
-<form action="index.php?ctrl=security&action=login" method="post">
-    
-    <!-- Champ d'entrée pour l'e-mail -->
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" required />
+<div class="login-container">
+  <div class="login-form">
+    <h3>Login</h3>
+    <form action="index.php?ctrl=security&action=login" method="post">
+      <!-- Champ d'entrée pour l'e-mail -->
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" required />
+      </div>
 
-    <!-- Champ d'entrée pour le mot de passe -->
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" required />
+      <!-- Champ d'entrée pour le mot de passe -->
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required />
+      </div>
 
-    <!-- Bouton de soumission du formulaire -->
-    <input class="button" type="submit" name="submit" id="submit" value="Login" />
-</form>
+      <!-- Bouton de soumission du formulaire -->
+      <input class="button" type="submit" name="submit" id="submit" value="Login" />
+    </form>
+  </div>
+</div>
