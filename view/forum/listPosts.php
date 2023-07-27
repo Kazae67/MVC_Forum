f795221e93545677450928a3a01266a60dd2f320
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-27 19:24:16 +0200

    label categorie en dehors, séparation + centrer

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -10,14 +10,19 @@ if (isset($_SESSION["user"]) && in_array($_SESSION["user"]->getRole(), ['admin',
 }
 ?>
 
+<!-- Catégorie -->
+<h2 class="post-category-centered">
+    <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$topic->getCategory()->getId()?>"><?=$topic->getCategory()->getCategoryLabel()?></a>
+</h2>
+
 <div class="post-container">
     <div class="post-header">
         <div class="post-header-left author-content">
-            <h2 class="post-category">
-                <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$topic->getCategory()->getId()?>"><?=$topic->getCategory()->getCategoryLabel()?></a>
-            </h2>
-            <h1 class="post-title">Title: <?=$topic->getTitle()?></h1>
-            <h2 class="post-topic-description">Description: <?=$topic->getTopic_description()?></h2>
+            <!-- Titre -->
+            <h2 class="post-title">Title: <?=$topic->getTitle()?></h2>
+            <hr>
+            <!-- Description -->
+            <h2 class="post-topic-description"><?=$topic->getTopic_description()?></h2>
         </div>
         <div class="post-header-right">
             <?php 
