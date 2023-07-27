36295622e6a604e3df2422db86e5cf1285a32e12
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-27 16:22:22 +0200

    ajout des filtres dans mes inputs

--- controller/TopicController.php
+++ controller/TopicController.php
@@ -8,7 +8,6 @@ use App\ControllerInterface;
 use Model\Managers\CategoryManager;
 use Model\Managers\TopicManager;
 use Model\Managers\PostManager;
-use Model\Managers\Manager;
 
 class TopicController extends AbstractController implements ControllerInterface
 {
@@ -62,56 +61,51 @@ class TopicController extends AbstractController implements ControllerInterface
             Session::addFlash('error', 'You need to log in to create a new topic.');
             $this->redirectTo('topic', 'listTopicsByCategory', $id);
         }
-    
-        $data = [];
+
         // Initialiser le résultat
         $result = null;
 
         // Si la requête est de type POST, nous devons créer un nouveau topic
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Stocker débogage
-            $_SESSION['debug']['post'] = $_POST;
-    
-            // Vérifier si l'ID de l'utilisateur est défini dans $_SESSION['user']
-            if ($_SESSION['user']->getId()) {
-                // Récupérer l'ID de l'utilisateur
-                $userId = $_SESSION['user']->getId();
-    
-                // Récupérer les données du formulaire
-                $data = [
-                    'title' => $_POST['title'],
-                    'topic_description' => $_POST['topic_description'],
-                    'user_id' => $userId,
-                    'category_id' => $id
-                ];
-    
-                // Appeler la méthode du gestionnaire pour créer le topic
-                $result = $this->topicManager->add($data);
-    
-                // Débogage result
-                $_SESSION['debug']['result'] = $result;
-    
-                // Checker si la création du topic est passée
-                if ($result == null) {
-                    Session::addFlash('error', 'There was an error creating your new topic.');
-                } else {
-                    Session::addFlash('success', 'Your new topic has been successfully created.');
-                }
-    
-                // Rediriger vers la liste des topics de cette catégorie
-                $this->redirectTo('topic', 'listTopicsByCategory', $id);
-            } else {
-                // Si l'ID de l'utilisateur n'est pas défini, afficher un message d'erreur
-                Session::addFlash('error', 'User ID not found.');
-                $this->redirectTo('topic', 'listTopicsByCategory', $id);
+            // Valider et filtrer les données du formulaire
+            $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+            $topicDescription = filter_input(INPUT_POST, 'topic_description', FILTER_SANITIZE_STRING);
+            $userId = $_SESSION['user']->getId();
+            $categoryId = (int) $id;
+
+            // Vérifier si les données sont valides
+            if (empty($title) || empty($topicDescription)) {
+                Session::addFlash('error', 'Please fill in all the required fields.');
+                $this->redirectTo('topic', 'newTopic', $id);
             }
+
+            // Utiliser des requêtes préparées pour éviter les attaques par injection SQL
+            $data = [
+                'title' => $title,
+                'topic_description' => $topicDescription,
+                'user_id' => $userId,
+                'category_id' => $categoryId
+            ];
+
+            // Appeler la méthode du gestionnaire pour créer le topic en utilisant une requête préparée
+            $result = $this->topicManager->add($data);
+
+            // Vérifier si la création du topic s'est bien déroulée
+            if (!$result) {
+                Session::addFlash('error', 'There was an error creating your new topic.');
+            } else {
+                Session::addFlash('success', 'Your new topic has been successfully created.');
+            }
+
+            // Rediriger vers la liste des topics de cette catégorie
+            $this->redirectTo('topic', 'listTopicsByCategory', $id);
         }
-    
+
         // Si la requête n'est pas de type POST, nous affichons simplement le formulaire
         else {
             $categoryManager = new CategoryManager();
             $category = $categoryManager->findOneById($id);
-    
+
             return [
                 "view" => VIEW_DIR . "forum/newTopic.php",
                 "data" => [
@@ -134,7 +128,7 @@ class TopicController extends AbstractController implements ControllerInterface
         $result = $this->topicManager->lockTopicById($id);
 
         // Si le topic est verrouillé avec succès, affiche un message de succès, sinon affiche un message d'erreur
-        if($result) {
+        if ($result) {
             Session::addFlash('success', 'The subject has been successfully locked.');
         } else {
             Session::addFlash('error', 'An error occurred while locking the subject.');
@@ -157,7 +151,7 @@ class TopicController extends AbstractController implements ControllerInterface
         $result = $this->topicManager->unlockTopicById($id);
 
         // Si le topic est déverrouillé avec succès, affiche un message de succès, sinon affiche un message d'erreur
-        if($result) {
+        if ($result) {
             Session::addFlash('success', 'The subject has been successfully unlocked.');
         } else {
             Session::addFlash('error', 'An error occurred when unlocking the subject.');
@@ -167,17 +161,14 @@ class TopicController extends AbstractController implements ControllerInterface
         $this->redirectTo('topic', 'listTopicsByCategory', $id);
     }
 
-
-
     // Prototype, supprimer si perte de temps
     public function listAllTopics()
     {
         $topics = $this->topicManager->findAll(["id", "DESC"]);
-    
+
         return [
             "view" => VIEW_DIR . "forum/listTopics.php",
             "data" => compact('topics')
         ];
     }
-    
 }
