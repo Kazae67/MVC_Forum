86115db458e8f599df61aef4408b6024728a850c
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-27 19:12:09 +0200

    ajout author content pour differencier de ceux qui répondent

--- view/forum/listPosts.php
+++ view/forum/listPosts.php
@@ -12,12 +12,12 @@ if (isset($_SESSION["user"]) && in_array($_SESSION["user"]->getRole(), ['admin',
 
 <div class="post-container">
     <div class="post-header">
-        <div class="post-header-left">
+        <div class="post-header-left author-content">
             <h2 class="post-category">
                 <a href="index.php?ctrl=topic&action=listTopicsByCategory&id=<?=$topic->getCategory()->getId()?>"><?=$topic->getCategory()->getCategoryLabel()?></a>
             </h2>
-            <h1 class="post-title">/<?=$topic->getTitle()?></h1>
-            <h2 class="post-topic-description">/<?=$topic->getTopic_description()?></h2>
+            <h1 class="post-title">Title: <?=$topic->getTitle()?></h1>
+            <h2 class="post-topic-description">Description: <?=$topic->getTopic_description()?></h2>
         </div>
         <div class="post-header-right">
             <?php 
