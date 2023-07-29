e353a6135c467db3ac9617f74bb30fabef411dc7
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-29 16:51:59 +0200

    gros changement css pour listTopics ainsi que footer & autres

--- view/layout.php
+++ view/layout.php
@@ -7,44 +7,45 @@
     <script src="https://kit.fontawesome.com/a45e9c27c8.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap">
+
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
     <title>FORUM</title>
 </head>
 <body>
     <div id="wrapper"> 
         <div id="mainpage"></div>
+        <header>
+            <nav>
+                <div id="nav-list">
+                    <a href="index.php?ctrl=category&action=listCategories">
+                    </a>
+                    <a class="button" href="index.php?ctrl=topic&action=listAllTopics">Topics list</a>
+                    <a class="button" href="index.php?ctrl=category&action=listCategories">Categories list</a>
+                </div>
+                <div id="nav-user">
+                    <?php if ($user = App\Session::getUser()): ?>
+                        <a class="button" href="index.php?ctrl=security&action=myProfile"><?= $user->getNickname() ?></a>
+                        <a class="button" href="index.php?ctrl=security&action=logOut">Logout</a>
+                    <?php else: ?>
+                        <a class="button" href="index.php?ctrl=security&action=login">Login</a>
+                        <a class="button" href="index.php?ctrl=security&action=register">Register</a>
+                    <?php endif; ?>
+                </div>
+            </nav>
             <!-- Message de succes ou d'erreur -->
             <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
             <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
-            <header>
-                <nav>
-                    <div id="nav-list">
-                        <a href="index.php?ctrl=category&action=listCategories">
-                        </a>
-                <a class="button" href="index.php?ctrl=topic&action=listAllTopics">Topics list</a>
-                <a class="button" href="index.php?ctrl=category&action=listCategories">Categories list</a>
-                    </div>
-                    <div id="nav-user">
-                        <?php if ($user = App\Session::getUser()): ?>
-                            <a class="button" href="index.php?ctrl=security&action=myProfile"><?= $user->getNickname() ?></a>
-                            <a class="button" href="index.php?ctrl=security&action=logOut">Logout</a>
-                        <?php else: ?>
-                            <a class="button" href="index.php?ctrl=security&action=login">Login</a>
-                            <a class="button" href="index.php?ctrl=security&action=register">Register</a>
-                        <?php endif; ?>
-                    </div>
-                </nav>
-            </header>
-            
-            <main id="forum">
-                <?= $page ?>
-            </main>
-        </div>
-        <footer>
-            <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html">Règlement du forum</a> - <a href="">Mentions légales</a></p>
-            <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
-        </footer>
+        </header>
+        
+        <main id="forum">
+            <?= $page ?>
+        </main>
     </div>
+    <footer>
+        <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html">Règlement du forum</a> - <a href="">Mentions légales</a></p>
+        <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
+    </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function(){
