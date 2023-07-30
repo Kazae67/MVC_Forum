3236c859f0415f57606d1ae2ac2d2e87ff30f18f
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-30 20:55:52 +0200

    amélioration commentaire entité Topic pour la compréhension

--- model/entities/Topic.php
+++ model/entities/Topic.php
@@ -7,46 +7,57 @@ use DateTime;
 
 final class Topic extends Entity
 {
-    private int $id;
-    private string $title;
+    // Propriétés de la classe
+    private int $id; 
+    private string $title; 
     private DateTime $topic_creation_date;
-    private bool $is_locked;
-    private $user;
+    private bool $is_locked; 
+    private $user; 
     private $category;
     private string $format;
-    private string $topic_description;
-    protected $countPost;
+    private string $topic_description; 
+    protected $countPost; 
     private ?string $lastActivity; 
 
+    // Constructeur de la classe
     public function __construct(array $data)
     {
         $this->hydrate($data);
+
+        // Si la date de création du topic est fournie, on l'instancie en objet DateTime.
+        // Sinon, on utilise la date et l'heure actuelles.
         if (isset($data['topic_creation_date'])) {
             $this->setTopic_creation_date(new DateTime($data['topic_creation_date']));
         } else {
             $this->topic_creation_date = new DateTime();
         }
-        if (isset($data['topic_description'])) {
-            $this->setTopic_description($data['topic_description']);
-        } else {
-            $this->topic_description = ""; 
-        }
+
+        // Si la description du topic est fournie, on la définit.
+        // Sinon, on initialise la description à une chaîne vide.
+        $this->topic_description = $data['topic_description'] ?? "";
+        
+        // Si le topic est verrouillé, on définit le booléen correspondant.
+        // Sinon, le topic n'est pas verrouillé (false).
         $this->is_locked = isset($data['is_locked']) ? (bool) $data['is_locked'] : false;
+        
+        // On récupère la dernière activité du topic (s'il y en a une) ou on définit null.
         $this->lastActivity = $data['lastActivity'] ?? null; 
     }
     
-    
     /**
-     * Get the value of id
-     */ 
+     * Méthode pour récupérer l'identifiant du topic.
+     * 
+     * @return int 
+     */
     public function getId(): int
     {
         return $this->id;
     }
 
     /**
-     * Set the value of id
+     * Méthode pour définir l'identifiant du topic.
      *
+     * @param int $id 
      * @return self
      */ 
     public function setId(int $id): self
@@ -56,7 +67,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of title
+     * Méthode pour récupérer le titre du topic.
+     * 
+     * @return string 
      */ 
     public function getTitle(): string
     {
@@ -64,8 +77,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of title
+     * Méthode pour définir le titre du topic.
      *
+     * @param string $title 
      * @return self
      */ 
     public function setTitle(string $title): self
@@ -75,7 +89,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of topic_creation_date
+     * Méthode pour récupérer la date de création du topic.
+     * 
+     * @return DateTime 
      */ 
     public function getTopic_creation_date(): DateTime
     {
@@ -83,8 +99,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of topic_creation_date
+     * Méthode pour définir la date de création du topic.
      *
+     * @param DateTime $date 
      * @return self
      */ 
     public function setTopic_creation_date(DateTime $date): self
@@ -94,7 +111,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of is_locked
+     * Méthode pour savoir si le topic est verrouillé.
+     * 
+     * @return bool 
      */ 
     public function getIs_locked(): bool
     {
@@ -102,8 +121,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of is_locked
+     * Méthode pour définir si le topic est verrouillé.
      *
+     * @param bool $is_locked 
      * @return self
      */ 
     public function setIs_locked(bool $is_locked): self
@@ -113,7 +133,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of user
+     * Méthode pour récupérer l'utilisateur associé au topic.
+     * 
+     * @return mixed 
      */ 
     public function getUser()
     {
@@ -121,8 +143,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of user
+     * Méthode pour définir l'utilisateur associé au topic.
      *
+     * @param mixed $user 
      * @return self
      */ 
     public function setUser($user): self
@@ -132,7 +155,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of category
+     * Méthode pour récupérer la catégorie à laquelle le topic appartient.
+     * 
+     * @return mixed 
      */ 
     public function getCategory()
     {
@@ -140,8 +165,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of category
+     * Méthode pour définir la catégorie à laquelle le topic appartient.
      *
+     * @param mixed $category 
      * @return self
      */ 
     public function setCategory($category): self
@@ -151,7 +177,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of format
+     * Méthode pour récupérer le format du topic.
+     * 
+     * @return string 
      */ 
     public function getFormat(): string
     {
@@ -159,8 +187,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of format
+     * Méthode pour définir le format du topic.
      *
+     * @param string $format 
      * @return self
      */ 
     public function setFormat(string $format): self
@@ -170,17 +199,19 @@ final class Topic extends Entity
     }
 
     /**
-     * Get the value of topic_description
+     * Méthode pour récupérer la description du topic.
+     * 
+     * @return string|null 
      */
     public function getTopic_description(): ?string
     {
         return $this->topic_description;
     }
     
-
     /**
-     * Set the value of topic_description
+     * Méthode pour définir la description du topic.
      * 
+     * @param string $topic_description 
      * @return self
      */
     public function setTopic_description(string $topic_description): self
@@ -189,20 +220,32 @@ final class Topic extends Entity
         return $this;
     }
 
-    // Méthode pour définir le nombre de posts
-    public function setCountPost($countPost)
+    /**
+     * Méthode pour définir le nombre de posts associés au topic.
+     * 
+     * @param int $countPost 
+     * @return self
+     */
+    public function setCountPost(int $countPost): self
     {
         $this->countPost = $countPost;
+        return $this;
     }
 
-    // Méthode pour récupérer le nombre de posts
-    public function getCountPost()
+    /**
+     * Méthode pour récupérer le nombre de posts associés au topic.
+     * 
+     * @return int 
+     */
+    public function getCountPost(): int
     {
         return $this->countPost;
     }
 
     /**
-     * Get the value of lastActivity
+     * Méthode pour récupérer la date de la dernière activité du topic.
+     * 
+     * @return string|null 
      */ 
     public function getLastActivity(): ?string
     {
@@ -210,8 +253,9 @@ final class Topic extends Entity
     }
 
     /**
-     * Set the value of lastActivity
+     * Méthode pour définir la date de la dernière activité du topic.
      *
+     * @param string|null $lastActivity 
      * @return self
      */ 
     public function setLastActivity(?string $lastActivity): self
@@ -219,8 +263,5 @@ final class Topic extends Entity
         $this->lastActivity = $lastActivity;
         return $this;
     }
-    
-
 }
-
 ?>
