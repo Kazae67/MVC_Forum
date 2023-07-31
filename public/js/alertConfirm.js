104c7179fc0131b494eb93e78eefc6e090b208a9
Kazae67 <Kazae.zaeka@gmail.com>
2023-07-31 04:41:39 +0200

    ajout de barre déroulante + alert ajouté dans role & ban unban + alertConfirm.js  + commentaires

---
+++ public/js/alertConfirm.js
@@ -0,0 +1,32 @@
+// Attendre que la page soit chargée avant d'exécuter le code JS
+window.onload = function() {
+    // Sélectionne tous les éléments avec la classe 'confirm'
+    var elements = document.querySelectorAll('.confirm');
+
+    // Parcours tous les éléments sélectionnés
+    for (var i = 0; i < elements.length; i++) {
+        // Si l'élément est un lien (<a>), ajoute un gestionnaire d'événements 'onclick'
+        if (elements[i].tagName === 'A') {
+            elements[i].onclick = function() {
+                // Lorsque l'utilisateur clique sur le lien, affiche une boîte de dialogue de confirmation
+                // Si l'utilisateur clique sur 'OK', le lien sera suivi
+                // Si l'utilisateur clique sur 'Annuler', le lien ne sera pas suivi
+                return confirm('Are you sure you want to ' + this.dataset.action + ' this user?');
+            }
+        }
+        // Si l'élément est une liste déroulante (<select>), ajoute un gestionnaire d'événements 'onchange'
+        else if (elements[i].tagName === 'SELECT') {
+            elements[i].onchange = function() {
+                // Lorsque l'utilisateur change la sélection, vérifie si une option autre que 'Select role' a été sélectionnée
+                // Si c'est le cas, affiche une boîte de dialogue de confirmation
+                if (this.value && confirm('Are you sure you want to ' + this.dataset.action + ' this user?')) {
+                    // Si l'utilisateur clique sur 'OK', soumet le formulaire en cliquant sur le bouton de soumission caché
+                    document.getElementById('roleSubmit').click();
+                } else {
+                    // Si l'utilisateur clique sur 'Annuler', réinitialise la sélection à 'Select role'
+                    this.value = '';
+                }
+            }
+        }
+    }
+}
--- view/security/usersProfiles.php
+++ view/security/usersProfiles.php
@@ -1,3 +1,4 @@
+<script src="public/js/alertConfirm.js"></script>
 <?php
 // Vérifie si la clé "user" existe dans $result["data"] et stocke sa valeur dans $user
 $user = isset($result["data"]['user']) ? $result["data"]['user'] : null; 
@@ -14,18 +15,26 @@ $userBanned = $user && $user->getBan() == 1;
 
 <?php if ($admin && !$userBanned): ?>
     <!-- Affiche un lien pour bannir l'utilisateur -->
-    <a class="button-red" href="index.php?ctrl=security&action=banUser&id=<?= $user->getId() ?>">Ban this user</a>
+    <a class="button-red confirm" data-action="ban" href="index.php?ctrl=security&action=banUser&id=<?= $user->getId() ?>">Ban this user</a>
 <?php elseif ($admin && $userBanned): ?>
     <!-- Affiche un lien pour débannir l'utilisateur -->
-    <a class="button-green" href="index.php?ctrl=security&action=unbanUser&id=<?= $user->getId() ?>">Unban this user</a>
+    <a class="button-green confirm" data-action="unban" href="index.php?ctrl=security&action=unbanUser&id=<?= $user->getId() ?>">Unban this user</a>
 <?php endif; ?>
 
 <?php if ($admin): ?>
-    <!-- Affiche un lien pour attribuer le rôle "user" à l'utilisateur -->
-    <a class="button-blue" href="index.php?ctrl=security&action=setUserRole&id=<?= $user->getId() ?>">Set as User</a>
-    <!-- Affiche un lien pour attribuer le rôle "admin" à l'utilisateur -->
-    <a class="button-blue" href="index.php?ctrl=security&action=setAdminRole&id=<?= $user->getId() ?>">Set as Admin</a>
+    <!-- Affiche une liste déroulante pour changer le rôle de l'utilisateur -->
+    <form action="index.php" method="get">
+        <input type="hidden" name="ctrl" value="security">
+        <input type="hidden" name="id" value="<?= $user->getId() ?>">
+        <select class="confirm" data-action="change role of" name="action" id="roleSelect">
+            <option value="">Select role</option>
+            <option value="setUserRole">Set as User</option>
+            <option value="setAdminRole">Set as Admin</option>
+        </select>
+        <button type="submit" id="roleSubmit" style="display: none;">Submit</button>
+    </form>
 <?php endif; ?>
+
 
 <?php if ($user): ?>
     <!-- Affiche les informations de l'utilisateur -->
